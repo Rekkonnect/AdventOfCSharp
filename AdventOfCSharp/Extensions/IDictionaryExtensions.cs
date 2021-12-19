acf98891374dedfc2cfb9ca723b226b454391f91
Alex <kalfas.alex@gmail.com>
2021-12-19 02:21:27 +0200

    Remove unrelated component leftovers

--- AdventOfCSharp/Extensions/IDictionaryExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AdventOfCSharp.Extensions;
-
-public static class IDictionaryExtensions
-{
-    public static IDictionary<TKey, bool> ToAvailabilityDictionary<TKey>(this IEnumerable<TKey> source) => source.ToDictionary(Selectors.SelfObjectReturner, _ => true);
-    public static IDictionary<TKey, TValue> ToDefaultValueDictionary<TKey, TValue>(this IEnumerable<TKey> source) => source.ToDictionary(Selectors.SelfObjectReturner, _ => default(TValue));
-}
