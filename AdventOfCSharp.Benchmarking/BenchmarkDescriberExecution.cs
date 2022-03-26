c7259bb0ede80f4463bc476b31a74f81d46954a4
Alex <kalfas.alex@gmail.com>
2022-03-26 22:09:00 +0200

    Fix input loading

---
+++ AdventOfCSharp.Benchmarking/BenchmarkDescriberExecution.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace AdventOfCSharp.Benchmarking;
+
+public static class BenchmarkDescriberExecution
+{
+    public static void RunBenchmark<T>(string customProblemFileBaseDirectory)
+    {
+        var previousBaseDirectory = ProblemFiles.CustomBaseDirectory;
+        ProblemFiles.SetCustomBaseDirectorySyncEnvironmentVariable(customProblemFileBaseDirectory);
+        BenchmarkRunner.Run<T>();
+        ProblemFiles.RestoreBaseDirectoryClearEnvironmentVariable(previousBaseDirectory);
+    }
+}
--- AdventOfCSharp.BenchmarkingPlayground/Program.cs
+++ AdventOfCSharp.BenchmarkingPlayground/Program.cs
@@ -1,5 +1,7 @@
-using AdventOfCSharp;
+using AdventOfCSharp.Benchmarking;
 using AdventOfCSharp.BenchmarkingPlayground;
-using BenchmarkDotNet.Running;
 
-BenchmarkRunner.Run<Consumer>();
+// This will be migrated to AoC#.Template for showcase
+// Additionally, docs should be included for explaining how to make use of this tooling
+const string path = @"custom path";
+BenchmarkDescriberExecution.RunBenchmark<Consumer>(path);
