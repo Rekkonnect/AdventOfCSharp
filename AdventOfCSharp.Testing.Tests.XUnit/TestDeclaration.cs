ccab0255a150ec2d3590aff101530eebdb9d63d0
Alex <kalfas.alex@gmail.com>
2022-03-31 17:40:41 +0300

    Transform TestingPlayground

---
+++ AdventOfCSharp.Testing.Tests.XUnit/TestDeclaration.cs
@@ -0,0 +1,6 @@
+using AdventOfCSharp.Testing;
+using AdventOfCSharp.Testing.Tests.Common;
+
+[assembly: AoCSTestAssembly(AoCSTestAssemblyInfo.ProblemFileBaseDirectory)]
+
+namespace AdventOfCSharp.Testing.Tests.NUnit;
--- AdventOfCSharp.TestingPlayground/AoCSTestingAssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using AdventOfCSharp.Testing;
-
-[assembly: AoCSTestAssembly(AoCSTestAssemblyInfo.ProblemFileBaseDirectory)]
-
-internal static class AoCSTestAssemblyInfo
-{
-    public const string ProblemFileBaseDirectory = @"C:\Users\Rekkon\Desktop\Projects\AdventOfCSharp\AdventOfCSharp.ProblemSolutionResources";
-}
