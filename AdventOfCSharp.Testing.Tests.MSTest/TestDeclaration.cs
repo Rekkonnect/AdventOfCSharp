77dc853e2e5f65180112ed8c45cf6a5b487962e6
Alex <kalfas.alex@gmail.com>
2022-03-31 17:44:28 +0300

    Add new tests to workflow

--- AdventOfCSharp.Testing.Tests.MSTest/TestDeclaration.cs
+++ AdventOfCSharp.Testing.Tests.MSTest/TestDeclaration.cs
@@ -2,5 +2,3 @@ using AdventOfCSharp.Testing;
 using AdventOfCSharp.Testing.Tests.Common;
 
 [assembly: AoCSTestAssembly(AoCSTestAssemblyInfo.ProblemFileBaseDirectory)]
-
-namespace AdventOfCSharp.Testing.Tests.NUnit;
